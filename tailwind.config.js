d9349da7bf7ba3b25b8dbd1b1aa7e869abf509b9
games-online-free <irhamnury@gmail.com>
2024-09-08 16:22:48 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"], // Update to match your actual file structure
+  content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('@tailwindcss/postcss7-compat'), // Jika menggunakan PostCSS versi 7
-  ],
-};
+  plugins: [],
+}
